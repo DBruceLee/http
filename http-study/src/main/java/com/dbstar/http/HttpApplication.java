fdface6800135ac2cae12734c9a4c4994264ac2d
DBruceLee <1204504782>
2020-04-24 23:52:31 +0800

    初始化提交

--- http-study/src/main/java/com/dbstar/http/HttpApplication.java
+++ http-study/src/main/java/com/dbstar/http/HttpApplication.java
@@ -1,9 +1,14 @@
 package com.dbstar.http;
 
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,9 @@ public class HttpApplication {
     }
 
     @RequestMapping("/hello")
-    public String test() {
+    public String test(HttpServletResponse response) {
         System.out.println("hello");
+        response.addHeader("Last-Modified", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         return "hello world";
     }
 
