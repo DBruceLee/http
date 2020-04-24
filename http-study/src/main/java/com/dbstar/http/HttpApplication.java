d3fc3f8f2fd1d26b008af8de878c34e8c6a11ebc
DBruceLee <1204504782>
2020-04-24 22:05:00 +0800

    初始化提交

--- http-study/src/main/java/com/dbstar/http/HttpApplication.java
+++ http-study/src/main/java/com/dbstar/http/HttpApplication.java
@@ -2,16 +2,21 @@ package com.dbstar.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController("/api")
 public class HttpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HttpApplication.class, args);
     }
 
-
+    @RequestMapping("/hello")
+    public String test() {
+        System.out.println("hello");
+        return "hello world";
+    }
 
 }
